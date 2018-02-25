75462ae222d640e2ae9dcccdd77b800626336a32
Jirapong Bualuang <jbualuang21@gmail.com>
2018-02-25 23:23:22 +0700

    Change icon.

--- app/src/main/java/jbualuang/com/incomeexpense/MainActivity.java
+++ app/src/main/java/jbualuang/com/incomeexpense/MainActivity.java
@@ -9,12 +9,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Fabric.with(this, new Crashlytics());
+
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
+
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
@@ -65,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+        int pid = android.os.Process.myPid();
+        android.os.Process.killProcess(pid);
     }
 }
